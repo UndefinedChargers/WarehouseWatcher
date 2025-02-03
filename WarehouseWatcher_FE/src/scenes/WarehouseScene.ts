bc46af216f1321d7473f02d4468050733bb28ab0
YJ <ypark0861@conestogac.on.ca>
2025-02-03 13:06:24 -0500

    Added glow layer, textblock with hover

--- WarehouseWatcher_FE/src/scenes/WarehouseScene.ts
+++ WarehouseWatcher_FE/src/scenes/WarehouseScene.ts
@@ -84,12 +84,12 @@ export class WarehouseScene {
       const hit = this.scene.pickWithRay(ray);
       if (hit.pickedMesh != null)
       {
-        // console.log(hit.pickedMesh.name);
+        console.log(hit.pickedMesh.name);
         // console.log(this.camera._currentTarget);
-        if (hit.pickedMesh.name == "thermo1")
-        {
-          this.DisplayGUI(hit.pickedMesh.name);
-        }
+        // if (hit.pickedMesh.name == "thermo1")
+        // {
+        //   this.DisplayGUI(hit.pickedMesh.name);
+        // }
       }
     };
   }
@@ -99,15 +99,68 @@ export class WarehouseScene {
   //
   // !! make argument string aray later
   CreateEnvironmentSensor(): void {
-    var hl = new BABYLON.HighlightLayer("hl", this.scene);    
-    var sensor_material = new BABYLON.StandardMaterial("mat", this.scene);
+    const topic = "Waterloo/Warehouse/Thermostat/Room"
+    const envstore = useEnvTopicStore();
     const sensor = this.scene.getMeshById("thermo1");
+    var sensor_material = new BABYLON.StandardMaterial("mat", this.scene);
+    // var hl = new BABYLON.HighlightLayer("hl", this.scene);    
+    // hl.addMesh(sensor.subMeshes[0].getRenderingMesh(), new BABYLON.Color3(0,1,0))
+    var gl = new BABYLON.GlowLayer("gl", this.scene);
+    gl.addIncludedOnlyMesh(sensor.subMeshes[0].getRenderingMesh());
+
+    var adt = GUI.AdvancedDynamicTexture.CreateFullscreenUI("guitext");
+    adt.useInvalidateRectOptimization = false;
+
+    let sensor_text_rect = new GUI.Rectangle();
+    adt.addControl(sensor_text_rect);
+    sensor_text_rect.width = "150px";
+    sensor_text_rect.height = "100px";
+    sensor_text_rect.linkOffsetY = "-100px";
+    sensor_text_rect.transformCenterY = 1;
+    // sensor_text_rect.background = "red"
+    sensor_text_rect.background = "#00000050";
+    sensor_text_rect.color = "#02132450";
+    sensor_text_rect.thickness = 1;
+    // sensor_text_rect.alpha = 0.8;
+    sensor_text_rect.scaleX = 0;
+    sensor_text_rect.scaleY = 0;
+    sensor_text_rect.linkWithMesh(sensor);
+
+    let sensor_text = new GUI.TextBlock();
+    sensor_text.fontSize = 50;
+    sensor_text.color = "white";
+    sensor_text.textWrapping = true;
+    sensor_text.textVerticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+    // sensor_text.textVerticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    sensor_text_rect.addControl(sensor_text);
+
+    let am_sesnor = new BABYLON.ActionManager(this.scene);
+    sensor.actionManager = am_sesnor;
+    console.log(this.scene);
+
+    am_sesnor.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, function(e){
+      // console.log(e.meshUnderPointer.name);
+      let topicdata = envstore.individualTopicData(topic)?.data.Data;
+      sensor_text.text = topicdata;
+      sensor_text_rect.scaleX = 0.5;
+      sensor_text_rect.scaleY = 0.5;
+      sensor_text.color = "green";
+
+      // this.scene.beginAnimation(sensor_text_rect, 0, 10, false);
+    }));
+
+    am_sesnor.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOutTrigger, function(e){
+      // this.scene.beginAnimation(sensor_text_rect, 10, 0, false);
+      sensor_text_rect.scaleX = 0;
+      sensor_text_rect.scaleY = 0;
+    }));
+
 
     this.scene.onAfterRenderObservable.add(() => {
       this.DisplayEnvironmentSensor(sensor, sensor_material);  
     }, 250)
   }
-
+ 
   //
   //
   //
