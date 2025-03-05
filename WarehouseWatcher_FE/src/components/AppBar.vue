5838f7c42a0b7752b84e2f495799ee52547bdff8
YJ <ypark0861@conestogac.on.ca>
2025-03-05 09:23:26 -0500

    Added notification, logo

--- WarehouseWatcher_FE/src/components/AppBar.vue
+++ WarehouseWatcher_FE/src/components/AppBar.vue
@@ -6,39 +6,41 @@
  DESCRIPTION: 
  References: Starting code- https://vuetifyjs.com/en/components/app-bars/#images
  -->
- <template>
-    
-      <v-layout>
-        <v-app-bar
-          color="#021324"
-          class="v-toolbar v-toolbar--flat"
-        >
-          <!-- <template v-slot:prepend>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
-          </template> -->
-  
-          <v-app-bar-title @click="$router.push({path:'/'})">WAREHOUSE WATCHER</v-app-bar-title>
-  
-          <v-spacer></v-spacer>
-          <v-btn @click="$router.push({path:'/dashboard'})"> 
-            Dashboard
-          </v-btn>
-          <v-btn @click="$router.push({path:'/space'})"> 
-            Space
-          </v-btn>
-          <v-btn @click="$router.push({path:'/compliance'})">
-            Compliance
-          </v-btn>
-          <v-btn @click="$router.push({path:'/about'})">
-            About
-          </v-btn>
-        </v-app-bar>
-      </v-layout>
-    
-  </template>
+<template>   
+  <v-layout>
+    <v-app-bar color="#021324" class="v-toolbar v-toolbar--flat">
+      <!-- <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template> -->
+      <img class="ww-logo" src="/favicon.ico"> 
+      <v-app-bar-title @click="$router.push({path:'/'})">WAREHOUSE WATCHER</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="$router.push({path:'/about'})">
+        About
+      </v-btn>
+      <v-btn @click="$router.push({path:'/dashboard'})"> 
+        Dashboard
+      </v-btn>
+      <v-btn @click="$router.push({path:'/space'})">
+        Space
+      </v-btn>
+      <v-btn @click="$router.push({path:'/compliance'})">
+        Compliance
+      </v-btn>
+      <v-btn class="text-none" stacked>
+        <v-badge color="error" content="2">
+          <v-icon>mdi-bell-outline</v-icon>
+        </v-badge>
+      </v-btn>
+    </v-app-bar>
+  </v-layout>
+</template>
+
+<script>
+
+</script>
 
 <style scoped>
-
 .app-bar {
   top: 0px;
   z-index: 1010;
@@ -46,5 +48,15 @@
   position: fixed;
   left: 0px;
   width: calc(100% - 0px);
+}
+
+.btn-alert {
+  color: white;
+}
+
+.ww-logo {
+  width:60px;
+  height:50px;
+  padding-left: 10px;
 }
 </style>
