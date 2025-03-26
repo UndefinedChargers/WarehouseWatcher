1cb309f8ad5f1963422df273b50cc29d6bdb78a2
amel <amelsunil28@gmail.com>
2025-03-26 02:54:05 -0400

    BE-environment simulation- fixed thermostat(temperature) responsive bug for UI element

--- WarehouseWatcher_BE/SimulatorService/src/main.py
+++ WarehouseWatcher_BE/SimulatorService/src/main.py
@@ -18,11 +18,6 @@ user =os.getenv("HIVEMQ_USER")
 password = os.getenv("HIVEMQ_PASS")
 host = os.getenv("HIVEMQ_HOST")
 
-# TOPICS={
-#     "thermostat": "Waterloo/Warehouse/Thermostat/",
-#     "AirQualitySensor":"Waterloo/Warehouse/AirQualitySensor/"
-
-# }
 
 # format for sensors
 # location :thermostat(sensor_name,temp_range,drain_cycle)
@@ -78,7 +73,7 @@ def on_control_message(client, userdata, msg):
             elif action == "increase_data":
                 field = payload.get("field")
 
-                if field == "Temperature" and hasattr(sensor, "set_increment_temp_one"):
+                if field == "Data" and hasattr(sensor, "set_increment_temp_one"):
                     
                     sensor.set_increment_temp_one()
                     logger.info(f"[CONTROL] Increased Temperature for {sensor_name} to {sensor.manual_temperature}")
@@ -101,7 +96,7 @@ def on_control_message(client, userdata, msg):
             elif action == "decrease_data":
                 
                 field = payload.get("field")
-                if field == "Temperature" and hasattr(sensor, "set_decrement_temp_one"):
+                if field == "Data" and hasattr(sensor, "set_decrement_temp_one"):
                     sensor.set_decrement_temp_one()
                     logger.info(f"[CONTROL] Decreased Temperature for {sensor_name} to {sensor.manual_temperature}")
                 
@@ -146,6 +141,15 @@ def publish_data(client):
          "sensor_name":sensor_name,
          "data":final_result
       }
+      # changes for the battery
+    # If battery is 0 or battery has been disabled via UI
+      if hasattr(sensor_instance, "battery") and (sensor_instance.battery == 0 or getattr(sensor_instance, "set_battery_UI", False)):final_result = {key: 0 for key in final_result}
+      logger.warning("Battery dead!!!!!!!!!!")
+      sensor_Data = {
+                "sensor_name": sensor_name,
+                "data": final_result
+            }
+
       sensor_type = sensor_instance.__class__.__name__ 
       theTopic = f"Waterloo/Warehouse/{sensor_type}/{sensor_name}"
       payload = json.dumps(sensor_Data)
